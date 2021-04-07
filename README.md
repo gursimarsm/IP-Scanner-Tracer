b176dc854d74132409b51a4338adb14816dfc6d7
Gursimar Singh <70017872+gursimarh@users.noreply.github.com>
2021-04-07 16:45:44 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# IP Scanner and Tracer
+
+<br>
+
+## IP Scanner
+Scan the IP addresses using the ipscan.sh shell script. It will scan all the ctive IP addresses in a network.
+Syntax ./ipscan.sh 192.168.99
+
+<br>
+
+## IP Tracer
+Use iptracer.py to trace the details about any IP address.
+The python script tracks the whearabouts of an IP Address. The Script prints out all the details of the currently used IP Address.
+This script takes IP address as argument and uses the IP location API to extract details realted to that IP Address.
+
---
+++ iptracer.py
@@ -0,0 +1,46 @@
+import requests
+import argparse
+import json
+
+# Making a function of get_ip to increase resuability and also adding some condiiional checks for error and boundary conditions
+
+def get_ip(ipAddress):									#taking IP address
+	try:
+		url = "http://ip-api.com/json/"+ ipAddress			        #appending IP to domain
+		response = requests.get(url)						#getting Response of the Query
+		data = json.loads(response.content)					#Getting useful data from the response that we get
+		if data['status'] == "fail":
+			return "Fail"
+		else:
+			return data
+	except Exception as e:
+		return "Exception Ocurred: {}".format(e)
+		
+
+def main():
+	parser = argparse.ArgumentParser()					# Argument parser to accept arguments when running script from a shell
+	parser.add_argument("ipaddress", help = "IP Address To Track")		# Made IP Address a compulsory argument
+	args = parser.parse_args()
+  functions-and-error-handling
+	data = get_ip(args.ipaddress)
+	if (data == "Fail") or ("Exception" in data):
+		print(data)
+		return
+	else:
+		if data.get("zip") == "":
+			data["zip"] = "Not Found / Not Applicable"
+		print("[+] IP Found\n[+] Fetching Details....\n")
+		print("\t[+] IP \t", data.get("query", "Not Found")) 				# Adding the dict.get() method to display a Not found message in case of any field not found or blank					#
+		print("\t[+] CITY \t", data.get("city", "Not Found"))				
+		print("\t[+] ISP \t", data.get("isp", "Not Found"))				
+		print("\t[+] LOC \t", data.get("country", "Not Found"))				
+		print("\t[+] REG \t", data.get("regionName", "Not Found"))			# Printing required data about given IP Address
+		print("\t[+] TIME \t", data.get("timezone", "Not Found"))			
+		print("\t[+] ZIP \t", data.get("zip", "Not Found"))					
+		print("\t[+] LAT \t", data.get("lat", "Not Found"))					
+		print("\t[+] LON \t", data.get("lon", "Not Found"))	
+
+
+# calling main method
+if __name__ == "__main__":
+	main()
